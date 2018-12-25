158de6cdb730f0ee79e2608301c378ff2e64141e
Kamburugamuwa K.L.A.P.T.J <it17092548@my.sliit.lk>
2018-12-25 22:09:45 +0530

    complete the search function and form filling function.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,68 @@
 var myNinjaApp =angular.module('myNinjaApp',[]);
 
-myNinjaApp.config(function(){
 
 
+myNinjaApp.controller('NinjaController',['$scope',function($scope){
 
-});
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja,1);
 
-myNinjaApp.run(function(){
 
+  };
+  $scope.addNinja= function(){
+      $scope.ninjas.push({
+        name:$scope.newninja.name,
+        belt:$scope.newninja.belt,
+        rate:parseInt($scope.newninja.rate),
+        available :false
+      });
+  };
 
-});
+  $scope.ninjas=[
+    {
+      name :"YOSHI",
+      belt:"green",
+      rate :50,
+      available:false
 
-myNinjaApp.controller('NinjaController',function($scope){
+    },
+    {
+      name :"Pasan",
+      belt:"red",
+      rate :50,
+      available:true
 
-  $scope.message="Hey y'all";
-  $scope.ninjas =['pasan','shameen','kasun','supun'];
+    },
+    {
+      name :"Naveen",
+      belt:"yellow",
+      rate :50,
+      available:true
 
-});
+    },
+    {
+      name :"A.u",
+      belt:"green",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"Naveen",
+      belt:"green",
+      rate :50,
+      available:true
+
+    },
+    {
+      name :"Charith",
+      belt:"pink",
+      rate :50,
+      available:true
+
+    }
+
+  ];
+
+}]);
